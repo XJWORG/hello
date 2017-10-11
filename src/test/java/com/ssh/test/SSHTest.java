3c9c3006ba70004111ed590b02b7736ffa46a808
xjw <xjw86@sohu.com>
2017-10-11 21:41:13 +0800

    配置spring junit注解

---
+++ src/test/java/com/ssh/test/SSHTest.java
@@ -0,0 +1,24 @@
+package com.ssh.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import javax.annotation.Resource;
+import org.junit.Test;
+import java.util.Date;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration(locations="classpath:beans.xml")  
+public class SSHTest {  
+      
+    @Resource  
+    private Date date;  
+    
+    
+    @Test //测试Spring IOC的开发环境  
+    public void springIoc() {  
+        System.out.println(date);  
+    } 
+}  
