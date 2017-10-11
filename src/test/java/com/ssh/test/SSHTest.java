9585cd57a4517a025e1692c1b77e3e9433b3a543
xjw <xjw86@sohu.com>
2017-10-11 22:05:49 +0800

    搭建spring环境完成

--- src/test/java/com/ssh/test/SSHTest.java
+++ src/test/java/com/ssh/test/SSHTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import java.util.Date;
 
 
-
 @RunWith(SpringJUnit4ClassRunner.class)  
 @ContextConfiguration(locations="classpath:beans.xml")  
 public class SSHTest {  
@@ -19,6 +18,7 @@ public class SSHTest {
     
     @Test //测试Spring IOC的开发环境  
     public void springIoc() {  
+        System.out.println(date);  
         System.out.println(date);  
     } 
 }  
