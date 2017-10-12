0f32fb92d7929be1f829ba074b771c0ceb465347
徐俊伟 <xujunwei@cmos.chinamobile.com>
2017-10-12 20:01:22 +0800

    添加struts2配置，其中shop新增比较有趣，务必注意
    <package name="shop" extends="struts-default">
            <action name="category_*" class="com.shop.action.CategoryAction"
    method="{1}">
                <result name="index">/index.jsp</result>
            </action>
        </package>

---
+++ src/main/java/com/shop/action/CategoryAction.java
@@ -0,0 +1,24 @@
+package com.shop.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.shop.service.CategoryService;
+
+public class CategoryAction extends ActionSupport {
+    private CategoryService categoryService;
+
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    
+    public String update(){
+        System.out.println("---struts update");
+        System.out.println(categoryService);
+        return "index";
+    }
+    
+    public String save(){
+        System.out.println("---struts save");
+        System.out.println(categoryService);
+        return "index";
+    }
+}
