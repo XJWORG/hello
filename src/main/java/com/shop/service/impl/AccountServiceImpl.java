96dc2ea9a404d6d761ea52a4842dfcbab7fb07ea
xjw <xjw86@sohu.com>
2017-10-13 22:05:56 +0800

    service抽取 - 新增一个Account表的实例

---
+++ src/main/java/com/shop/service/impl/AccountServiceImpl.java
@@ -0,0 +1,10 @@
+package com.shop.service.impl;
+
+import java.util.List;
+
+import com.shop.model.Account;
+import com.shop.service.AccountService;
+
+public class AccountServiceImpl extends BaseServiceImpl<Account> implements AccountService {
+
+}
